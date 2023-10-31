003ae3c3c0c6adcb6eade90f205bebd4e8787a36
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-10-31 12:55:35 -0400

    fix: catch build errors

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 sourceDir=$(cd $(dirname ${BASH_SOURCE[0]:-$0}) && pwd -P)
 buildDir=build-iguana
 [ $# -gt 0 ] && installDir=$1 || installDir=iguana
@@ -7,12 +9,10 @@ buildDir=build-iguana
 echo """
 sourceDir  = $sourceDir
 buildDir   = $buildDir
-installDir = $installDir"""
+installDir = $installDir
+"""
 
-if [ ! -d $buildDir ]; then
-  meson setup --prefix $installDir $buildDir $sourceDir
-else
+[ ! -d $buildDir ] && 
+  meson setup --prefix $installDir $buildDir $sourceDir ||
   meson configure $buildDir --prefix $installDir
-fi
-
 meson install -C $buildDir
