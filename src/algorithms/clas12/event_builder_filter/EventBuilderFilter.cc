177f4c158e351a5f310bb5d6a62b9b6b2bce99d9
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-11-14 15:51:01 -0500

    feat: attempt to write a new bank

--- src/algorithms/clas12/event_builder_filter/EventBuilderFilter.cc
+++ src/algorithms/clas12/event_builder_filter/EventBuilderFilter.cc
@@ -8,9 +8,29 @@ namespace iguana::clas12 {
 
   Algorithm::BankMap EventBuilderFilter::Run(Algorithm::BankMap inputBanks) {
 
-    if(MissingInputBanks(inputBanks, {"particles"})) ThrowRun();
+    // check the input banks existence
+    if(MissingInputBanks(inputBanks, {"particles"}))
+      ThrowRun();
 
-    return inputBanks;
+    // define the output schemata and banks
+    BankMap outputBanks = {
+      { "particles", hipo::bank(inputBanks.at("particles").getSchema(), inputBanks.at("particles").getRows()) }
+    };
+
+    // filter the input bank for requested PDG code(s)
+    int j=0;
+    for(int i=0; i<inputBanks.at("particles").getRows(); i++) {
+      auto pid = inputBanks.at("particles").get("pid",i);
+      m_log->Info("INPUT PID = {}", pid);
+      if(pid==11)
+        outputBanks.at("particles").put("pid", j++, pid);
+    }
+
+    // print
+    m_log->Info("SHOW OUTPUT BANKS");
+    outputBanks.at("particles").show();
+
+    return outputBanks;
   }
 
   void EventBuilderFilter::Stop() {
