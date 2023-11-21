680c51bf51f70cf4047c191b991c490c95013752
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-11-21 11:53:12 -0500

    fix: use `fmt::underlying`

--- src/services/Logger.h
+++ src/services/Logger.h
@@ -42,7 +42,7 @@ namespace iguana {
                   vals...
                   );
             } else {
-              Warn("Logger::Print called with unknown log level '{}'; printing as error instead", static_cast<int>(lev)); // FIXME: static_cast -> fmt::underlying, but needs new version of fmt
+              Warn("Logger::Print called with unknown log level '{}'; printing as error instead", fmt::underlying(lev));
               Error(message, vals...);
             }
           }
