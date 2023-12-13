4449227ef786e37cfac32417521a1af7052998bd
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-12-13 00:58:36 -0500

    fix: drop `fmt` from dependent builds

--- examples/build_with_make/test.sh
+++ examples/build_with_make/test.sh
@@ -5,14 +5,12 @@ set -e
 
 # paths to dependencies
 # - you will need to customize these
-# - if they are installed on your system, (e.g., `fmt`), you may not need to specify them
 # - this script is used by CI, which assumes they are in `./<dependency>`
 hipo_dep=$(realpath hipo)
-fmt_dep=$(realpath fmt)
 iguana_dep=$(realpath iguana)
 
 # set pkg-config path
-export PKG_CONFIG_PATH=$fmt_dep/lib/pkgconfig:$iguana_dep/lib/pkgconfig
+export PKG_CONFIG_PATH=$iguana_dep/lib/pkgconfig
 
 # hipo doesn't use pkg-config; instead just set a variable to its installation prefix
 export HIPO=$hipo_dep
@@ -28,5 +26,5 @@ popd
 # run the executable
 # - since we didn't set the rpath in the Makefile, we must use LD_LIBRARY_PATH
 # - passes script arguments to the executable
-export LD_LIBRARY_PATH=$hipo_dep/lib:$fmt_dep/lib:$iguana_dep/lib
+export LD_LIBRARY_PATH=$hipo_dep/lib:$iguana_dep/lib
 $source_dir/bin/iguana-example-00-basic "$@"
