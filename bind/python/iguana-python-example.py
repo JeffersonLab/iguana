14f15e9baadcbc1c5edfca6ddd01dae8183efd16
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-12-15 06:43:34 -0500

    feat: read a HIPO file with python and iguana

--- bind/python/iguana-python-example.py
+++ bind/python/iguana-python-example.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 
+import cppyy
 import PyIguana.AlgorithmSequence as iguana
 
-seq = iguana.AlgorithmSequence('PyIguana')
+cppyy.load_library('hipo4')
+cppyy.include('hipo4/reader.h')
+from cppyy.gbl import hipo
 
-algos = [
-        'clas12::EventBuilderFilter',
-        'clas12::LorentzTransformer',
-        ]
-for algo in algos:
-    seq.Add(algo)
+inFile    = 'data.hipo'
+numEvents = 3
+
+reader = hipo.reader(inFile)
+cppyy.cppexec('std::vector<std::string> banks = { "REC::Particle", "REC::Calorimeter" };')
+banks = reader.getBanks(cppyy.gbl.banks)
+
+seq = iguana.AlgorithmSequence('PyIguana')
+seq.Add('clas12::EventBuilderFilter')
+seq.Add('clas12::LorentzTransformer')
 seq.PrintSequence()
 
 # # TODO: need to marshal to std::variant; config files are a possible workaround here
-# for algo in algos:
-#     seq.SetOption(algo, 'log', 'trace')
+cppyy.cppexec('iguana::option_t log_level = "debug";')
+cppyy.cppexec('iguana::option_t pids = std::set<int>{11,211,-211};')
+seq.SetOption('clas12::EventBuilderFilter', 'log',  cppyy.gbl.log_level)
+seq.SetOption('clas12::EventBuilderFilter', 'pids', cppyy.gbl.pids)
+
+def prettyPrint(message, bank):
+    print(f'{"="*30} {message} {"="*30}')
+    bank.show()
+
+iEvent = 0
+seq.Start(banks)
+while(reader.next(banks) and (numEvents==0 or iEvent < numEvents)):
+    iEvent += 1
+    prettyPrint("BEFORE", banks[0])
+    seq.Run(banks)
+    prettyPrint("AFTER", banks[0])
+
+seq.Stop()
