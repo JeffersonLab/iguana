20af2c85f63e9e5316012829cf4354a15ce78e3e
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-05 12:12:11 -0500

    feat: generate a template algorithm (#67)

---
+++ src/iguana/algorithms/example/ExampleAlgorithm.h
@@ -0,0 +1,98 @@
+//############################################################################
+//# allow this header to be included only once
+//############################################################################
+#pragma once
+
+//############################################################################
+//# include `AlgorithmFactory`, so that we may register this algorithm for factory creation
+//############################################################################
+#include "iguana/algorithms/AlgorithmFactory.h"
+
+//############################################################################
+//# define the namespace
+//# - all `iguana` code should be within the `iguana` namespace
+//# - algorithms specific to an experiment or analysis may be put in a namespace within the `iguana` namespace
+//#   - here we use the `iguana::example` namespace
+//#   - for CLAS12-specific algorithms, use `iguana::clas12`
+//############################################################################
+namespace iguana::example {
+
+  //############################################################################
+  //# define the algorithm class
+  //# - it must inherit from `Algorithm`, which includes common methods and objects used by each algorithm
+  //# - it must also include a Doxygen docstring, typically defined by 3 forward slashes `///`;
+  //#   see Doxygen documentation for more details, or see other algorithms
+  //############################################################################
+  /// @brief This is a template algorithm; provide a brief, one-line description of your algorithm here.
+  ///
+  /// Provide a more detailed description of your algorithm here, if you want.
+  class ExampleAlgorithm : public Algorithm {
+
+    //############################################################################
+    //# this is a preprocessor macro call which generates boilerplate for the algorithm definition
+    //# - the arguments are:
+    //#   - the class name, `ExampleAlgorithm`
+    //#   - a unique, "full" name of the algorithm, used by `AlgorithmFactory`; typically is the
+    //#     namespace with the class name, excluding the `iguana::` part, but you are free to choose any name
+    //# - NOTE: quotes are not used, and there is no need for a semicolon at the end of this call
+    //# - see `../AlgorithmBoilerplate.h` for details
+    //#   - the macros are relatively modular, so if you want to use your own constructor or destructor, you may
+    //#     do so, and use other preprocessor macros called within `DEFINE_IGUANA_ALGORITHM` to complete
+    //#     the boilerplate public and private functions and members
+    //############################################################################
+    DEFINE_IGUANA_ALGORITHM(ExampleAlgorithm, example::ExampleAlgorithm)
+
+    public:
+
+      //############################################################################
+      //# public functions go here
+      //# - typically these are "action functions", which expose the primary operation of an algorithm
+      //# - these functions are _unique_ to each algorithm, and therefore are not defined in the
+      //#   `Algorithm` base class
+      //# - their purpose is to allow the usage of the algorithm for users who _don't_ process full banks,
+      //#   but rather process bank rows, or from some other data source
+      //# - try to keep the parameters and return types _friendly_ to language bindings; for example,
+      //#   avoid "complicated" types and lvalue references
+      //# - don't forget the Doxygen docstrings
+      //# - the action function here is trivial, just to show an example
+      //# - you do not have to name it as `Filter`, but take a look at other algorithms and try to
+      //#   keep some consistency, for example:
+      //#   - `bool Filter` for a filtering type algorithm, such as fiducial cuts
+      //#   - `Transform` for a transformation type algorithm, such as momentum corrections
+      //#   - `Create` for a creation type algorithm, such as inclusive kinematic (x, Q2, etc.) reconstruction
+      //############################################################################
+      /// **Action function**: checks if the PDG `pid` is positive;
+      /// this is an example action function, please replace it with your own
+      /// @param pid the particle PDG to check
+      /// @returns `true` if `pid` is positive
+      bool Filter(const int pid) const;
+
+    private:
+
+      //############################################################################
+      //# indices for the banks needed for this algorithm
+      //# - see `Algorithm::CacheBankIndex` for details
+      //# - here, we just define one for the `REC::Particle` bank
+      //# - convention: they should start with `b_`
+      //############################################################################
+      /// `hipo::banklist` index for the particle bank (as an example)
+      hipo::banklist::size_type b_particle;
+
+      //############################################################################
+      //# configuration options
+      //# - their type may be:
+      //#   - one of the allowed types in `option_t`, which is a `std::variant`
+      //#   - `std::set`, used by `Algorithm::CacheOptionToSet`, which converts
+      //#     a user's `std::vector` option to a `std::set`
+      //#   - your own type, but you will have to set it in the `Start()` method
+      //# - here we show example `int` and `float` options
+      //# - convention: they should start with `o_`
+      //############################################################################
+      /// Example integer configuration option
+      int o_testInt;
+      /// Example float configuration option
+      double o_testFloat;
+
+  };
+
+}
---
+++ src/iguana/algorithms/example/README.md
@@ -0,0 +1,25 @@
+# Example Algorithm Template
+
+This directory includes a fully documented, simple example algorithm:
+- [`ExampleAlgorithm.h`](ExampleAlgorithm.h)
+- [`ExampleAlgorithm.cc`](ExampleAlgorithm.cc)
+
+In this code, the following comment styles are used:
+- `//#`: this is a comment which describes in detail what each line of the code does; you probably
+  don't want this much detail in your own algorithm
+- `///`: this is a docstring for [Doxygen](https://www.doxygen.nl/), for automated documentation generation;
+  you'll need to use these when documenting your algorithm
+
+To generate a template algorithm (without the `//#` comments) so you may get
+started coding, run the `make_template.sh` script here:
+```bash
+# run with no arguments to see usage
+src/iguana/algorithms/example/make_template.sh
+
+# example: make a CLAS12 algorithm called AwesomeAlgorithm
+src/iguana/algorithms/example/make_template.sh AwesomeAlgorithm clas12 src/iguana/algorithms/clas12
+```
+
+Once you have generated your new algorithm, add it to the appropriate
+`meson.build` file (likely [`src/iguana/algorithms/meson.build`](../meson.build)),
+and get started coding!
