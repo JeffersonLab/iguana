7766a8e8773bb152c1f86bba9e85b725cd86752d
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-08 15:43:37 -0500

    test: does `$DYLD_LIBRARY_PATH` fix python bindings?

--- bind/python/pyiguana/__init__.py
+++ bind/python/pyiguana/__init__.py
@@ -6,8 +6,13 @@ import os, cppyy, pkgconfig
 for pkg in ['iguana'] + [p.split()[0] for p in pkgconfig.requires('iguana') if p!='']:
     if not pkgconfig.exists(pkg):
         raise Exception(f'failed to find "{pkg}.pc" in pkg-config path')
-    cppyy.add_include_path(pkgconfig.variables(pkg)['includedir'])
-    cppyy.add_library_path(pkgconfig.variables(pkg)['libdir'])
+    pkg_info = {
+            'includedir': pkgconfig.variables(pkg)['includedir'],
+            'libdir':     pkgconfig.variables(pkg)['libdir'],
+            }
+    print(f'DEBUG: pyiguana - add package {pkg}: {pkg_info}')
+    cppyy.add_include_path(pkg_info['includedir'])
+    cppyy.add_library_path(pkg_info['libdir'])
 
 # add libraries to cppyy
 for lib in ['hipo4', 'IguanaServices', 'IguanaAlgorithms']:
