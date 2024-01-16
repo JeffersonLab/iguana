d9bcdc035747fd380e4904d4551e4e97418c0488
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-16 10:21:37 -0500

    fix: cleanup `pyiguana` init

--- bind/python/pyiguana/__init__.py
+++ bind/python/pyiguana/__init__.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
-import os, cppyy, pkgconfig
+import cppyy, pkgconfig
 
 # add include and library directories to cppyy
 for pkg in ['iguana'] + [p.split()[0] for p in pkgconfig.requires('iguana') if p!='']:
     if not pkgconfig.exists(pkg):
         raise Exception(f'failed to find "{pkg}.pc" in pkg-config path')
-    pkg_info = {
-            'includedir': os.path.realpath(pkgconfig.variables(pkg)['includedir']),
-            'libdir':     os.path.realpath(pkgconfig.variables(pkg)['libdir']),
-            }
-    print(f'DEBUG: pyiguana - add package {pkg}: {pkg_info}')
-    cppyy.add_include_path(pkg_info['includedir'])
-    cppyy.add_library_path(pkg_info['libdir'])
+    cppyy.add_include_path(pkgconfig.variables(pkg)['includedir'])
+    cppyy.add_library_path(pkgconfig.variables(pkg)['libdir'])
 
 # add libraries to cppyy
 for lib in ['hipo4', 'IguanaServices', 'IguanaAlgorithms']:
