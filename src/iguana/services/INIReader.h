f3fa3d2db39e50ceea0569c65594b7d4dd9fb6f5
rtysonCLAS12 <tyson@exppatricka-mbp.jlab.org>
2024-01-25 18:23:53 -0500

    Added documentation for INIReader

--- src/iguana/services/INIReader.h
+++ src/iguana/services/INIReader.h
@@ -21,8 +21,20 @@ namespace iguana {
       /// Get the file opened by INIReader
       std::string GetFile() const;
 
+      /// Read a value from the opened INI file which is at a given section and key.
+      /// This function can take in any C++ type.
+      /// @param section the value's section in the INI file.
+      /// @param key the value's key in the section in the INI file.
+      /// @param defaultValue the function will default to this if the section or key do not exist.
+      /// @returns the value at the section and key in the INI file.
       template <typename T> T readValue(const std::string &section, const std::string &key, T defaultValue);
 
+      /// Read an array from the opened INI file which is at a given section and key.
+      /// This function can take in any C++ type.
+      /// @param section the array's section in the INI file.
+      /// @param key the array's key in the section in the INI file.
+      /// @param defaultValue the function will default to this if the section or key do not exist.
+      /// @returns the array at the section and key in the INI file returned as a std::vector.
       template <typename T> std::vector<T> readArray(const std::string &section, const std::string &key, const std::vector<T> &defaultValue);
 
     protected:
@@ -30,7 +42,7 @@ namespace iguana {
       /// The file to be opened by INIReader
       std::string m_file;
 
-      // Create a GKeyFile
+      /// GKeyFile used to open file
       GKeyFile *m_keyfile;
   };
 }
