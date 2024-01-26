5db28d6776584e0719784074bbb5d7d5ffa19e2a
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-26 18:31:14 -0500

    Revert "ci: send version info directly to summary stream"
    This reverts commit 06b597d1e9e2015bceeddd9ee21540d83ba9d970.

--- .github/install-dependency-packages.sh
+++ .github/install-dependency-packages.sh
@@ -20,13 +20,16 @@ runner=$1
 
 ##################################
 
+summary_file=pkg_summary.md
+> $summary_file
+
 if [[ "$runner" =~ "macos" ]]; then
   echo "[+] On macOS runner"
   for pkg in ${PACKAGE_LIST_MACOS[@]}; do
     echo "[+] INSTALLING $pkg"
     brew install $pkg
     if [ ! "$pkg" = "tree" ]; then
-      echo "| \`$pkg\` | $(brew info $pkg | head -n1) |" >> $GITHUB_STEP_SUMMARY
+      echo "| \`$pkg\` | $(brew info $pkg | head -n1) |" >> $summary_file
     fi
   done
 elif [[ "$runner" =~ "ubuntu" ]]; then
@@ -34,7 +37,7 @@ elif [[ "$runner" =~ "ubuntu" ]]; then
   for pkg in ${PACKAGE_LIST_LINUX[@]}; do
     echo "[+] INSTALLING $pkg"
     sudo apt -y install $pkg
-    echo "| \`$pkg\` | $(apt show $pkg | grep -E '^Version:') |" >> $GITHUB_STEP_SUMMARY
+    echo "| \`$pkg\` | $(apt show $pkg | grep -E '^Version:') |" >> $summary_file
   done
 else
   echo "ERROR: runner '$runner' is unknown to $0" >&2
