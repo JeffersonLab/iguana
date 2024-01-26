43349b56ef2409cd8c954c40b4fa0937ad799917
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-26 18:21:39 -0500

    fix(ci): install `yaml-cpp`

---
+++ .github/install-dependency-packages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+PACKAGE_LIST_MACOS=(
+  fmt
+  yaml-cpp
+  tree
+)
+
+PACKAGE_LIST_LINUX=(
+  yaml-cpp
+)
+
+##################################
+
+if [ $# -ne 1 ]; then
+  echo "USAGE: $0 [runner]" >&2
+  exit 2
+fi
+runner=$1
+
+##################################
+
+summary_file=pkg_summary.md
+> $summary_file
+
+if [[ "$runner" =~ "macos" ]]; then
+  echo "[+] On macOS runner"
+  for pkg in ${PACKAGE_LIST_MACOS[@]}; do
+    echo "[+] INSTALLING $pkg"
+    brew install $pkg
+    if [ ! "$pkg" = "tree" ]; then
+      echo "| \`$pkg\` | $(brew info $pkg | head -n1) |" >> $summary_file
+    fi
+  done
+elif [[ "$runner" =~ "ubuntu" ]]; then
+  echo "[+] On Linux runner"
+  for pkg in ${PACKAGE_LIST_LINUX[@]}; do
+    echo "[+] INSTALLING $pkg"
+    apt -y install $pkg
+    echo "| \`$pkg\` | $(apt show $pkg | grep -E '^Version:') |" >> $summary_file
+  done
+else
+  echo "ERROR: runner '$runner' is unknown to $0" >&2
+  exit 1
+fi
