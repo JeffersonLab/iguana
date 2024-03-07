b1c568af7859879f76c1b2bb4140302c5afebab0
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-03-07 11:57:09 -0500

    fix(SectorFinder): only define bank lvalues if used

--- src/iguana/algorithms/clas12/SectorFinder.cc
+++ src/iguana/algorithms/clas12/SectorFinder.cc
@@ -1,4 +1,5 @@
 #include "SectorFinder.h"
+#include <cassert>
 
 namespace iguana::clas12 {
 
@@ -43,53 +44,56 @@ namespace iguana::clas12 {
   std::vector<int> SectorFinder::Find(hipo::banklist& banks) const
   {
     std::vector<int> sectors;
-    // get the banks
     auto& particleBank = GetBank(banks, b_particle, "REC::Particle");
-    auto& userBank=GetBank(banks, b_user);
-    auto& calBank=GetBank(banks, b_calorimeter);
-    auto& scintBank=GetBank(banks,b_scint);
-    auto& trackBank=GetBank(banks,b_track);
 
     // filter the input bank for requested PDG code(s)
-    for(int row = 0; row < particleBank.getRows(); row++) {
-      if(userSpecifiedBank){
-        //if user specified a specific bank
+    if(userSpecifiedBank) { // if user specified a specific bank
+      auto& userBank=GetBank(banks, b_user);
+      for(int row = 0; row < particleBank.getRows(); row++) {
         if(userBank.getRows()>0){
           sectors.push_back(getSector(userBank,row));
         } else{
           //bank may be empty
           sectors.push_back(0);
-        } 
-      } else{
-          int trackSector=0;
-          if(trackBank.getRows()>0){
-            trackSector=getSector(trackBank,row);
-          } 
-          
-          int scintSector=0;
-          if(scintBank.getRows()>0){
-            scintSector=getSector(scintBank,row);
-          }
-
-          int calSector=0;
-          if(calBank.getRows()>0){
-            calSector=getSector(calBank,row);
-          } 
-          
-          if(trackSector!=0){
-            sectors.push_back(trackSector);
-          } else if(scintSector!=0){
-            sectors.push_back(scintSector);
-          } else{
-            //add even if calSector is 0
-            //need an entry per pindex
-            //can happen that particle not in FD
-            //ie sector is 0
-            sectors.push_back(calSector);
-          } 
-          
         }
+      }
     }
+    else { // use the standard method
+      auto& calBank=GetBank(banks, b_calorimeter);
+      auto& scintBank=GetBank(banks,b_scint);
+      auto& trackBank=GetBank(banks,b_track);
+      for(int row = 0; row < particleBank.getRows(); row++) {
+        int trackSector=0;
+        if(trackBank.getRows()>0){
+          trackSector=getSector(trackBank,row);
+        }
+
+        int scintSector=0;
+        if(scintBank.getRows()>0){
+          scintSector=getSector(scintBank,row);
+        }
+
+        int calSector=0;
+        if(calBank.getRows()>0){
+          calSector=getSector(calBank,row);
+        }
+
+        if(trackSector!=0){
+          sectors.push_back(trackSector);
+        } else if(scintSector!=0){
+          sectors.push_back(scintSector);
+        } else{
+          //add even if calSector is 0
+          //need an entry per pindex
+          //can happen that particle not in FD
+          //ie sector is 0
+          sectors.push_back(calSector);
+        }
+      }
+    }
+
+    // verify results have the same size as `particleBank`
+    assert((sectors.size() == static_cast<decltype(sectors)::size_type>(particleBank.getRows())));
 
     return sectors;
   }
