0be7373139d7034c80a3d1e66f288f9af013f483
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-03-08 18:01:38 -0500

    doc: namespaces

--- doc/namespaces.h
+++ doc/namespaces.h
@@ -3,5 +3,11 @@
 /// General `iguana` namespace
 namespace iguana {}
 
-/// Namespace for CLAS12 `iguana` algorithms
+/// Namespace for example algorithms
+namespace iguana::example {}
+
+/// Namespace for CLAS12 algorithms
 namespace iguana::clas12 {}
+
+/// Namespace for physics algorithms
+namespace iguana::physics {}
