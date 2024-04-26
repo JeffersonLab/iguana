de5f27f96c2df19e7d2f533123d71523b3efa91b
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-04-26 13:42:10 -0400

    feat: generate modulefile

--- meson/release/install.sh
+++ meson/release/install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# automation for deploying new releases
+# this script is for maintainers, not users; users should
+# follow installation documentation instead
+
 set -e
 set -u
 
 sourceDir=$(realpath $(dirname ${BASH_SOURCE[0]:-$0})/../..)
 version=$($sourceDir/meson/detect-version.sh $sourceDir)
 echo """
-Detected version $version
-"""
+Detected version $version"""
 
 if [ $# -ne 2 ]; then
   print_var() { echo "$@" | sed 's/:/\n      /g'; }
   echo """
-  USAGE: $0 [BUILD_DIR] [INSTALL_PREFIX]
+  USAGE: $0 [BUILD_PREFIX] [INSTALL_PREFIX]
 
-    BUILD_DIR       local build directory
+    BUILD_PREFIX    build in BUILD_PREFIX-$version
     INSTALL_PREFIX  install to INSTALL_PREFIX/$version
 
   NOTE: ensure necessary dependencies are in:
@@ -27,17 +30,27 @@ if [ $# -ne 2 ]; then
   """
   exit 2
 fi
-buildDir=$1
+buildDir=$1-$version
 installDir=$(realpath $2)/$version
 nativeFile=$sourceDir/meson/release/native/release.ini
 echo """
 sourceDir  = $sourceDir
 buildDir   = $buildDir
 installDir = $installDir
-nativeFile = $nativeFile
 """
+echo "nativeFile = $nativeFile:"
+cat $nativeFile
+printf "\nProceed with installation? [y/N] "
+read proceed
+case $proceed in
+  y|Y|yes)
+    echo "Proceeding with installation..."
+    ;;
+  *)
+    echo "Abort."
+    exit 1
+    ;;
+esac
 
-exit
 meson setup $buildDir $sourceDir --native-file=$nativeFile --prefix=$installDir
 meson install -C $buildDir
-# TODO: generate module file
