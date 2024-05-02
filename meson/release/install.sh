bc363d8c6950bd596780b5936a82923d5fa303f8
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-05-02 19:45:07 -0400

    build: module file tagging

--- meson/release/install.sh
+++ meson/release/install.sh
@@ -31,19 +31,21 @@ sourceDir=$(realpath $(dirname ${BASH_SOURCE[0]:-$0})/../..)
 version=$($sourceDir/meson/detect-version.sh $sourceDir)
 msg "Detected version $version"
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
   echo """
-  USAGE: $0 [BUILD_PREFIX] [INSTALL_PREFIX] [TEST_HIPO_FILE]
+  USAGE: $0 [BUILD_PREFIX] [INSTALL_PREFIX] [TEST_HIPO_FILE] [TAG(optional)]
 
     BUILD_PREFIX    build in BUILD_PREFIX-$version
     INSTALL_PREFIX  install to INSTALL_PREFIX/$version
     TEST_HIPO_FILE  a sample HIPO file, for running tests
+    TAG             if specified, use TAG instead of version number ($version)
   """
   print_dep_vars
   exit 2
 fi
-buildDir=$1-$version
-installDir=$(realpath $2)/$version
+[ $# -ge 4 ] && tag=$4 || tag=$version
+buildDir=$1-$tag
+installDir=$(realpath $2)/$tag
 testFile=$(realpath $3)
 nativeFile=$sourceDir/meson/release/native/release.ini
 echo """
@@ -71,6 +73,7 @@ msg "meson setup"
 meson setup $buildDir $sourceDir \
   --native-file=$nativeFile      \
   --prefix=$installDir           \
+  -Dz_module_tag=$tag            \
   -Dtest_data_file=$testFile
 msg "meson install"
 meson install -C $buildDir
@@ -81,12 +84,12 @@ msg "Done installation"
 echo """
   prefix:      $installDir
 
-  moduleFile:  $buildDir/$version
+  moduleFile:  $buildDir/$tag
 """
 msg "Next steps:"
 echo """
 - [ ] export LD_LIBRARY_PATH=$installDir/lib:\$LD_LIBRARY_PATH
 - [ ] try running installed binaries: $installDir/bin/
 - [ ] copy the module file to the correct location
-- [ ] module switch iguana/$version
+- [ ] module switch iguana/$tag
 """
