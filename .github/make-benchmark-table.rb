f9f6c7ed0accfe7fc26a5f24e3cc1944be5d379a
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-07-03 17:17:32 -0400

    ci: repeat benchmarks and get average time

---
+++ .github/make-benchmark-table.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+if ARGV.empty?
+  $stderr.puts "USAGE: #{$0} [benchmark_output]"
+  exit 2
+end
+
+# parse the benchmark output
+benchmark_results = File.readlines(ARGV[0])
+  .grep(/.*benchmark-algorithm.*/)
+  .map(&:chomp)
+  .map do |line|
+    tokens = line.split
+    if tokens.size == 4
+      [
+        tokens[1],
+        tokens[3].gsub('s','').to_f
+      ]
+    else
+      nil
+    end
+  end.compact
+
+# collect the algorithms' execution times into a Hash
+benchmark_hash = Hash.new
+benchmark_results.each do |result|
+  name = result[0]
+    .gsub(/benchmark-algorithm-/, '')
+    .gsub(/-/, '::')
+  unless benchmark_hash.has_key? name
+    benchmark_hash[name] = Array.new
+  end
+  benchmark_hash[name] << result[1]
+end
+
+# print the table
+def row(arr)
+  puts '| ' + arr.join(' | ') + ' |'
+end
+row ['Algorithm', 'Average Time (s)']
+row ['---', '---']
+benchmark_hash.each do |name,times|
+  ave = times.sum / times.size
+  row [ "`#{name}`", ave ]
+end
