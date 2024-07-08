5199c6d5bf82406657c3c11c7fd8ca68e85b8a5c
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-07-08 18:21:25 -0400

    fix: Fortran bindings

--- src/iguana/algorithms/clas12/ZVertexFilter/Bindings.cc
+++ src/iguana/algorithms/clas12/ZVertexFilter/Bindings.cc
@@ -7,10 +7,11 @@ namespace iguana::bindings {
   /// @see `iguana::clas12::ZVertexFilter::Filter`
   /// @param [in] algo_idx the algorithm index
   /// @param [in] vz
+  /// @param [in] pid
   /// @param [in,out] out the return value
-  void iguana_clas12_zvertexfilter_filter_(algo_idx_t* algo_idx, float* vz, bool* out)
+  void iguana_clas12_zvertexfilter_filter_(algo_idx_t* algo_idx, float* vz, int* pid, bool* out)
   {
-    *out = *out && dynamic_cast<clas12::ZVertexFilter*>(iguana_get_algo_(algo_idx))->Filter(*vz);
+    *out = *out && dynamic_cast<clas12::ZVertexFilter*>(iguana_get_algo_(algo_idx))->Filter(*vz, *pid);
   }
   }
 }
