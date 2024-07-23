0b567ccd887e15cebc802470a94ca7130995d58a
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-07-23 19:50:55 -0400

    feat: get beam energy

--- src/iguana/services/RCDBReader.cc
+++ src/iguana/services/RCDBReader.cc
@@ -1,11 +1,29 @@
 #include "RCDBReader.h"
 
-#include <RCDB/Connection.h>
-
 namespace iguana {
 
   RCDBReader::RCDBReader(std::string_view name) : Object(name)
   {
+#ifdef USE_RCDB
+    auto url_ptr = std::getenv("RCDB_CONNECTION");
+    m_url = url_ptr != nullptr ? std::string(url_ptr) : default_url;
+    m_log->Debug("RCDB URL: {}", m_url);
+    m_rcdb_connection = std::make_unique<rcdb::Connection>(m_url, true);
+#endif
+  }
+
+  double RCDBReader::GetBeamEnergy(int const runnum)
+  {
+#ifdef USE_RCDB
+    auto cnd = m_rcdb_connection->GetCondition(runnum, "beam_energy");
+    if(!cnd)
+      throw std::runtime_error(fmt::format("Failed to find beam energy from RCDB for run {}", runnum));
+    return cnd->ToDouble();
+#else
+    double def = 10.6;
+    m_log->Warn("RCDB dependency not found; GetBeamEnergy will return default value of {} GeV.", def);
+    return def;
+#endif
   }
 
 }
--- src/iguana/services/RCDBReader.h
+++ src/iguana/services/RCDBReader.h
@@ -2,6 +2,10 @@
 
 #include "Object.h"
 
+#ifdef USE_RCDB
+#include <RCDB/Connection.h>
+#endif
+
 namespace iguana {
 
   /// @brief RCDB reader
@@ -12,6 +16,19 @@ namespace iguana {
 
       /// @param name the name of this reader
       RCDBReader(std::string_view name = "rcdb");
+
+      /// @param runnum run number
+      /// @returns the beam energy
+      double GetBeamEnergy(int const runnum);
+
+    private:
+
+      std::string m_url;
+      std::string const default_url = "mysql://rcdb@clasdb.jlab.org/rcdb";
+
+#ifdef USE_RCDB
+      std::unique_ptr<rcdb::Connection> m_rcdb_connection;
+#endif
 
   };
 }
