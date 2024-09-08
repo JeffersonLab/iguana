cd539513f5f4dac29f24ac762119cbf4596fcd75
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-09-08 13:52:13 -0400

    fix: move the maxver closer to the minver

--- meson/minimum-version.sh
+++ meson/minimum-version.sh
@@ -11,9 +11,10 @@ if [ $# -eq 0 ]; then
             repositories, we prefer to use the name from that used by the CI
 
   command:
-    choices:  ver  return the minimum version number
-              ALA  return a URL for the Arch Linux Archive (ALA), for CI
-              tag  return a git tag for the minimum version's source code
+    choices:  ver         return the minimum version number
+              ALA         return a URL for the Arch Linux Archive (ALA), for CI
+              tag         return a git tag for the minimum version's source code
+              tag_latest  return the latest git tag (that we test)
     default: $cmd
 
   ALA_mirror:  the ALA mirror, for command 'ALA'
@@ -37,21 +38,24 @@ case $dep in
     result_ver='9.1.0'
     result_ala="$ala_mirror/packages/f/fmt/fmt-9.1.0-4-x86_64.pkg.tar.zst"
     not_used 'tag'
+    not_used 'tag_latest'
     ;;
   yaml-cpp)
     result_ver='0.7.0'
     result_ala="$ala_mirror/packages/y/yaml-cpp/yaml-cpp-0.7.0-2-x86_64.pkg.tar.zst"
     not_used 'tag'
+    not_used 'tag_latest'
     ;;
   root|ROOT)
     result_ver='6.28.10'
-    result_tag='v6-28-10'
+    [ "$cmd" = "tag" ] && result_tag='v6-28-10' || result_tag='v6-32-04' # minver tag || maxver tag
     not_used 'ALA'
     ;;
   ruby)
     result_ver='2.7.2'
     not_used 'ALA'
     not_used 'tag'
+    not_used 'tag_latest'
     ;;
   *)
     echo "ERROR: dependency '$dep' is unknown" >&2
@@ -64,7 +68,7 @@ esac
 case $cmd in
   ver) echo $result_ver ;;
   ALA) echo $result_ala ;;
-  tag) echo $result_tag ;;
+  tag|tag_latest) echo $result_tag ;;
   *)
     echo "ERROR: command '$cmd' is unknown" >&2
     exit 1
