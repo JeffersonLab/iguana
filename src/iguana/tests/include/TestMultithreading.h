2b154d1d8dde1dfe7206d93bfb76f4c91eab7d44
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-09-25 18:11:37 -0400

    test: `Start` the algorithms

--- src/iguana/tests/include/TestMultithreading.h
+++ src/iguana/tests/include/TestMultithreading.h
@@ -67,91 +67,90 @@ inline int TestMultithreading(
   stream.open(data_file.c_str());
 
   // define the worker function
-  auto ftn = [](int order) { return 1; };
-  // auto ftn = [
-  //   &stream,
-  //   algo_name,
-  //   prerequisite_algos,
-  //   bank_names,
-  //   vary_run,
-  //   verbose,
-  //   num_events_per_thread,
-  //   num_events_per_frame,
-  //   run_config_bank_idx
-  // ](int order) {
-  //
-  //   // fill a frame
-  //   std::vector<hipo::event> events;
-  //   for(int i = 0; i < num_events_per_frame; i++)
-  //     events.push_back(hipo::event());
-  //
-  //   // bank list
-  //   hipo::banklist banks;
-  //   for(auto const& bank_name : bank_names)
-  //     banks.push_back(hipo::bank(stream.dictionary().getSchema(bank_name.c_str()),48));
-  //
-  //   // define the algorithm
-  //   iguana::AlgorithmSequence seq;
-  //   for(auto const& prerequisite_algo : prerequisite_algos)
-  //     seq.Add(prerequisite_algo);
-  //   seq.Add(algo_name);
-  //   seq.SetName("TEST thread " + std::to_string(order));
-  //   seq.PrintSequence();
-  //   seq.SetOption(algo_name, "log", verbose ? "trace" : "info");
-  //
-  //   // start the algorithm
-  //   seq.Start(banks);
-  //
-  //   // loop over frames
-  //   int nProcessed = 0;
-  //   while(nProcessed < num_events_per_thread || num_events_per_thread == 0) {
-  //     stream.pull(events);
-  //
-  //     // loop over events in this frame
-  //     int nNonEmpty = 0;
-  //     for(auto& event : events) {
-  //       if(event.getSize() > 16) {
-  //         nNonEmpty++;
-  //         nProcessed++;
-  //
-  //         // read the banks
-  //         for(auto& bank : banks)
-  //           event.read(bank);
-  //
-  //         // occasionally vary the run number; so far, algorithms with data-dependent configuration
-  //         // parameters have dependence on run number, so this variation aims to improve thread
-  //         // sanitizer test coverage
-  //         if(vary_run && run_config_bank_idx.has_value()) {
-  //           // === rapid variation ===
-  //           /*
-  //           banks[run_config_bank_idx.value()].putInt("run", 0, std::rand() % 3000);
-  //           */
-  //           // === slower variation ===
-  //           if(std::rand() % 10 == 0) { // randomly increase or decrease the run number
-  //             auto runnum = banks[run_config_bank_idx.value()].getInt("run", 0);
-  //             runnum += (std::rand() % 2 == 0) ? 1000 : -1000;
-  //             runnum = std::max(runnum, 0); // prevent negative run number
-  //             banks[run_config_bank_idx.value()].putInt("run", 0, runnum);
-  //           }
-  //           else if(std::rand() % 10 == 1) {
-  //             banks[run_config_bank_idx.value()].putInt("run", 0, 1); // set the runnum to '1'
-  //           }
-  //         }
-  //
-  //         // run the iguana algorithm
-  //         seq.Run(banks);
-  //       }
-  //     }
-  //     if(nNonEmpty == 0)
-  //       break;
-  //   }
-  //
-  //   // stop the algorithm
-  //   seq.Stop();
-  //
-  //   seq.GetLog()->Info("nProcessed = {}", nProcessed);
-  //   return nProcessed;
-  // };
+  auto ftn = [
+    &stream,
+    algo_name,
+    prerequisite_algos,
+    bank_names,
+    vary_run,
+    verbose,
+    num_events_per_thread,
+    num_events_per_frame,
+    run_config_bank_idx
+  ](int order) {
+
+    // fill a frame
+    std::vector<hipo::event> events;
+    for(int i = 0; i < num_events_per_frame; i++)
+      events.push_back(hipo::event());
+
+    // bank list
+    hipo::banklist banks;
+    for(auto const& bank_name : bank_names)
+      banks.push_back(hipo::bank(stream.dictionary().getSchema(bank_name.c_str()),48));
+
+    // define the algorithm
+    iguana::AlgorithmSequence seq;
+    for(auto const& prerequisite_algo : prerequisite_algos)
+      seq.Add(prerequisite_algo);
+    seq.Add(algo_name);
+    seq.SetName("TEST thread " + std::to_string(order));
+    seq.PrintSequence();
+    seq.SetOption(algo_name, "log", verbose ? "trace" : "info");
+
+    // start the algorithm
+    seq.Start(banks);
+
+    // loop over frames
+    int nProcessed = 0;
+    // while(nProcessed < num_events_per_thread || num_events_per_thread == 0) {
+    //   stream.pull(events);
+    //
+    //   // loop over events in this frame
+    //   int nNonEmpty = 0;
+    //   for(auto& event : events) {
+    //     if(event.getSize() > 16) {
+    //       nNonEmpty++;
+    //       nProcessed++;
+    //
+    //       // read the banks
+    //       for(auto& bank : banks)
+    //         event.read(bank);
+    //
+    //       // occasionally vary the run number; so far, algorithms with data-dependent configuration
+    //       // parameters have dependence on run number, so this variation aims to improve thread
+    //       // sanitizer test coverage
+    //       if(vary_run && run_config_bank_idx.has_value()) {
+    //         // === rapid variation ===
+    //         /*
+    //         banks[run_config_bank_idx.value()].putInt("run", 0, std::rand() % 3000);
+    //         */
+    //         // === slower variation ===
+    //         if(std::rand() % 10 == 0) { // randomly increase or decrease the run number
+    //           auto runnum = banks[run_config_bank_idx.value()].getInt("run", 0);
+    //           runnum += (std::rand() % 2 == 0) ? 1000 : -1000;
+    //           runnum = std::max(runnum, 0); // prevent negative run number
+    //           banks[run_config_bank_idx.value()].putInt("run", 0, runnum);
+    //         }
+    //         else if(std::rand() % 10 == 1) {
+    //           banks[run_config_bank_idx.value()].putInt("run", 0, 1); // set the runnum to '1'
+    //         }
+    //       }
+    //
+    //       // run the iguana algorithm
+    //       seq.Run(banks);
+    //     }
+    //   }
+    //   if(nNonEmpty == 0)
+    //     break;
+    // }
+
+    // stop the algorithm
+    seq.Stop();
+
+    seq.GetLog()->Info("nProcessed = {}", nProcessed);
+    return nProcessed;
+  };
 
   // run
   stream.run(ftn, num_threads);
