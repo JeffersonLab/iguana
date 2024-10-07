b2d1d8b1a77980283a3aa99d8bae10274c8ba767
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-10-07 11:47:40 -0400

    doc: better docstrings

--- src/iguana/services/RCDBReader.h
+++ src/iguana/services/RCDBReader.h
@@ -10,6 +10,13 @@
 namespace iguana {
 
   /// @brief RCDB reader
+  ///
+  /// This class interfaces to the RCDB. The database connection path is chosen from one of the following, in order:
+  /// - The global variable `iguana::GlobalRcdbUrl`; by default this is not set to any value (its type is `iguana::GlobalParam`)
+  /// - The environment variable `RCDB_CONNECTION` (which is likely set if you are on `ifarm`)
+  /// - A default URL, which will be printed in a warning; see `iguana::RCDBReader::m_default_url`
+  ///
+  /// RCDB will automatically use `mariadb` / `mysql` or `sqlite`, depending on the RCDB path, and whether you have satisfied the dependencies.
   class RCDBReader : public Object
   {
 
@@ -23,10 +30,12 @@ namespace iguana {
       /// @returns the beam energy in GeV
       double GetBeamEnergy(int const runnum);
 
-    private:
+    protected:
 
       /// @brief default RCDB URL, used as a last resort
       std::string const m_default_url = "mysql://rcdb@clasdb.jlab.org/rcdb";
+
+    private:
 
       std::string m_url;
       std::once_flag m_error_once;
