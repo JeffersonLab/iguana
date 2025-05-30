ae809e75436e97bb53d52ce39499e2841a02f824
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-05-30 13:03:48 -0400

    feat: support `bool` configuration parameters

--- src/iguana/algorithms/Algorithm.h
+++ src/iguana/algorithms/Algorithm.h
@@ -23,6 +23,7 @@ namespace iguana {
   using option_t = std::variant<
       int,
       double,
+      bool,
       std::string,
       std::vector<int>,
       std::vector<double>,
@@ -191,6 +192,7 @@ namespace iguana {
       // PrintOptionValue: overloaded for different value types
       void PrintOptionValue(std::string const& key, int const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
       void PrintOptionValue(std::string const& key, double const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
+      void PrintOptionValue(std::string const& key, bool const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
       void PrintOptionValue(std::string const& key, std::string const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
       void PrintOptionValue(std::string const& key, std::vector<int> const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
       void PrintOptionValue(std::string const& key, std::vector<double> const& val, Logger::Level const level = Logger::debug, std::string_view prefix = "OPTION") const;
