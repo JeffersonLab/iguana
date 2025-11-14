3dff3661ef53a52968aced37a35a29bc764f31d2
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-11-14 16:11:57 -0500

    fix(doc): banklist documentation linking (#399)

--- src/iguana/algorithms/Algorithm.h
+++ src/iguana/algorithms/Algorithm.h
@@ -17,6 +17,7 @@ namespace iguana {
   // ALGORITHM TOOLS
   //////////////////////////////////////////////////////////////////////////////
 
+  /// general algorithm tools
   namespace tools {
     /// Get the index of a bank from a `hipo::banklist`.
     /// @note this function is preferred over `hipo::getBanklistIndex`, since it handles the case where there are more than one bank
@@ -26,7 +27,7 @@ namespace iguana {
     /// @param variant if 0, the _first_ bank named `bank_name` in `banks` will be returned; if 1, the 2nd such bank will be returned, _etc_.;
     /// note, you can call `Algorithm::GetCreatedBankVariant` to get the created-bank variant number for a specific algorithm
     /// @returns the `hipo::banklist` index of the bank
-    /// @see Algorithm::GetCreatedBankVariant to get the variant number for an algorithm's created bank(s), or use Algorithm::GetBankIndex
+    /// @see Algorithm::GetCreatedBankIndex or AlgorithmSequence::GetCreatedBankIndex to get the index of a bank created by a creator algorithm.
     hipo::banklist::size_type GetBankIndex(
         hipo::banklist& banks,
         std::string const& bank_name,
