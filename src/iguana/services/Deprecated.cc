de22905f4b8f35e6851ad7dd07c87f4f23e6ba34
Christopher Dilks <c-dilks@users.noreply.github.com>
2026-01-22 19:20:48 -0500

    fix: more renames

--- src/iguana/services/Deprecated.cc
+++ src/iguana/services/Deprecated.cc
@@ -9,13 +9,36 @@ namespace iguana::deprecated {
   {
     // clang-format off
     std::map<std::string,std::map<std::string,std::string>> renames = {
-      { "physics::InclusiveKinematics", {
-                                          { "reconstruction",  "method/reconstruction" },
-                                          { "lepton_finder",   "method/lepton_finder" },
-                                          { "beam_particle",   "method/beam_particle" },
-                                          { "beam_direction",  "initial_state/beam_direction" },
-                                          { "target_particle", "initial_state/target_particle" },
-                                        },
+      {
+        "clas12::rga::FiducialFilterPass2",
+        {
+          { "calorimeter.strictness",      "calorimeter/strictness" },
+          { "forward_tagger.radius",       "forward_tagger/radius" },
+          { "forward_tagger.holes_flat",   "forward_tagger/holes_flat" },
+          { "cvt.edge_layers",             "cvt/edge_layers" },
+          { "cvt.edge_min",                "cvt/edge_min" },
+          { "cvt.phi_forbidden_deg",       "cvt/phi_forbidden_deg" },
+          { "dc.theta_small_deg",          "dc/theta_small_deg" },
+          { "dc.thresholds_out",           "dc/thresholds_out" },
+          { "dc.thresholds_in_smallTheta", "dc/thresholds_in_smallTheta" },
+          { "dc.thresholds_in_largeTheta", "dc/thresholds_in_largeTheta" },
+        }
+      },
+      {
+        "clas12::ZVertexFilter",
+        {
+          {"electron_vz", "electron/vz"},
+        }
+      },
+      {
+        "physics::InclusiveKinematics",
+        {
+          { "reconstruction",  "method/reconstruction" },
+          { "lepton_finder",   "method/lepton_finder" },
+          { "beam_particle",   "method/beam_particle" },
+          { "beam_direction",  "initial_state/beam_direction" },
+          { "target_particle", "initial_state/target_particle" },
+        }
       },
     };
     // clang-format on
